e2367ecd37b12a424f97ca54fa9f2654821ba3b9
Gouse Alla Sajan Bandalachenu <bsajan960@gmail.com>
2025-11-04 14:24:58 +0530

    Sum of two numbers and Area of rectangle codes

---
+++ src/main/java/com/sajan/scaler/nov/SumofTwonumbers_areaOfRectangleCodes.java
@@ -0,0 +1,17 @@
+package com.sajan.scaler.nov;
+
+public class SumofTwonumbers_areaOfRectangleCodes {
+    public static void main(String[] args) {
+        // Sum of two numbers
+        int A = 5;
+        int B = 10;
+        int sum = A + B;
+        System.out.println("Sum of " + A + " and " + B + " is: " + sum);
+
+        // Area of Rectangle
+        int length = 7;
+        int breadth = 4;
+        int area = length * breadth;
+        System.out.println("Area of Rectangle with length " + length + " and breadth " + breadth + " is: " + area);
+    }
+}
